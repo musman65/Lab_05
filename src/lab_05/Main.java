c611d7ca807f5e6246e34a22d598e48eaaecaf1c
6298674 <6298674@host.docker.internal>
2025-10-06 08:24:04 -0400

    test commit, this adds nothing

---
+++ src/lab_05/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab_05;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author 6298674
+ */
+public class Main extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void start(Stage stage) {
+        //test test
+    }
+    
+}
