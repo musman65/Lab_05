d5ce09db23000cec0b08f37db51078838b2be71e
Usman Muhammad <68919810+musman65@users.noreply.github.com>
2025-10-06 10:45:29 -0400

    Started events

--- src/lab_05/Task2.java
+++ src/lab_05/Task2.java
@@ -9,6 +9,7 @@ package lab_05;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
@@ -34,12 +35,21 @@ public class Task2 extends Application {
     public void start(Stage stage) {
         stage.setTitle("Bill Calculator");
         
+        //Info
+        
+        
         //Labels and Buttons
         Label bev = new Label("Beverage");
         Label app = new Label("Appetizer");
         Label main = new Label("Main Course");
         Label des = new Label("Dessert");
         Label tipLabel = new Label("Tip (%):");
+        Label finalTipLabel = new Label("Tip Selected (%): 0");
+        Label totalWithoutTip = new Label("Total without tip: $0");
+        Label totalWithTip = new Label("Total with tip: $0");
+        Button calc = new Button("Calculae Total");
+        Button clear = new Button("Clear");  
+        Label warning = new Label("test");
         
         //Slider
         Slider tip = new Slider(0.0, 20.0, 0.0);
@@ -63,6 +73,61 @@ public class Task2 extends Application {
         mainCB.setPrefWidth(comboBoxWidth);
         desCB.setPrefWidth(comboBoxWidth);
         
+        //Events
+        calc.setOnMousePressed(event -> {
+            if (bevCB.getSelectionModel().getSelectedItem() == null) {
+                warning.setText("Please select a beverage!");
+                return;
+            }
+            
+            if (appCB.getSelectionModel().getSelectedItem() == null) {
+                warning.setText("Please select an appetizer!");
+                return;
+            }
+            
+            if (mainCB.getSelectionModel().getSelectedItem() == null) {
+                warning.setText("Please select a main course!");
+                return;
+            }
+            
+            if (desCB.getSelectionModel().getSelectedItem() == null) {
+                warning.setText("Please select a dessert!");
+                return;
+            }
+            
+            double total = 0;
+            
+            switch (bevCB.getSelectionModel().getSelectedItem()) {
+                case "Coffee" -> {
+                    total += 2.50;
+                }
+                case "Tea" -> {
+                    total += 2.00;
+                }
+                case "Soft Drink" -> {
+                    total += 1.75;
+                }
+                case "Water" -> {
+                    total += 2.95;
+                }
+                case "Milk" -> {
+                    total += 2.50;
+                }
+                case "Juice" -> {
+                    total += 2.50;
+                }
+                
+            }
+            
+            
+            warning.setText("");
+            
+        });
+        
+        clear.setOnMousePressed(event -> {
+            
+        });
+        
         //Panes
         BorderPane root = new BorderPane();
         GridPane gp = new GridPane();
@@ -79,16 +144,19 @@ public class Task2 extends Application {
         gp.add(des,         0, 3);
         gp.add(desCB,       1, 3);
         
-        VBox vb = new VBox(20, tipLabel, tip);
-        VBox vb2 = new VBox(40, gp, vb);
+        VBox tipVB = new VBox(20, tipLabel, tip);
+        VBox finalInfo = new VBox(20, finalTipLabel, totalWithoutTip, totalWithTip);
+        VBox vb2 = new VBox(40, gp, warning, tipVB, finalInfo);
         
         root.setCenter(vb2);
         
         //Padding and other customization
         root.setPadding(new Insets(20));
         gp.setHgap(50);
+        warning.getStyleClass().add("label-warning");
         
         Scene scene = new Scene(root, 500, 300);
+        scene.getStylesheets().add("styles.css");
         stage.setScene(scene);
         stage.show();
     }
